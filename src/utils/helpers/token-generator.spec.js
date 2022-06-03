d38e18568f1a88bc6186add23e26285cfc33d407
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2022-06-03 10:53:39 -0300

    ensure TokenGenerator returns null if JWT returns null

---
+++ src/utils/helpers/token-generator.spec.js
@@ -0,0 +1,14 @@
+
+class TokenGenerator {
+  async generate(id) {
+    return null
+  }
+}
+
+describe('Token generator', () => {
+  test('Should return null if JWT retruns null', async () => {
+    const sut = new TokenGenerator()
+    const token = await sut.generate('any_id')
+    expect(token).toBeNull()
+  })
+})
