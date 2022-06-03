9da5c09e8a3daed69bcc463a3402960eb83256a5
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2022-06-03 11:14:40 -0300

    ensure TokenGenerator thows if no id is provided

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -9,7 +9,10 @@ class TokenGenerator {
   async generate(id) {
     if(!this.secret){
       throw new MissignParamError('secret')
-    }            
+    }    
+    if(!id){
+      throw new MissignParamError('id')
+    }          
     return jwt.sign(id, this.secret)
   }
 }
@@ -25,20 +28,29 @@ describe('Token generator', () => {
     const token = await sut.generate('any_id')
     expect(token).toBeNull()
   })
+
   test('Should return a token if JWT retruns token', async () => {
     const sut = makeSut()
     const token = await sut.generate('any_id')
     expect(token).toBe(jwt.token)
   })
+
   test('Should call JWT if correct values', async () => {
     const sut = makeSut()
     await sut.generate('any_id')
     expect(jwt.id).toBe('any_id')
     expect(jwt.secret).toBe(sut.secret)
   })
+
   test('Should throw if no secret is provided', async () => {
     const sut = new TokenGenerator()
     const promise = sut.generate('any_id')
     expect(promise).rejects.toThrow(new MissignParamError('secret'))
+  })
+
+  test('Should throw if no id is provided', async () => {
+    const sut = makeSut()
+    const promise = sut.generate()
+    expect(promise).rejects.toThrow(new MissignParamError('id'))
   })
 })
